c122fe2016861872fce245ee10ee9b1cb65647d0
paul <paul@tomandco.co.uk>
2016-10-05 15:52:21 +0200

    Added server and html files

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 // https://zellwk.com/blog/crud-express-mongodb/
+
+
+const express = require('express');
+const app = express();
+
+app.listen(3000, () => {
+    console.log( 'listening on 300' );
+})
+
+app.get('/', (req, res) => {
+    res.sendFile( __dirname + '/index.html' );
+})
+
+app.post('/quotes', (req, res) => {
+    console.log(req.route.stack);
+    res.sendFile(__dirname + '/thanks.html')
+})
+
