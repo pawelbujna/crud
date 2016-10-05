68359831882f3d1ad5786af80fae788964e37c48
Pawel Bujna <pawelbujna@pc4.home>
2016-10-06 01:10:56 +0200

    Added body parser and mongo client. Connected with DB

--- server.js
+++ server.js
@@ -2,18 +2,38 @@
 
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 
-app.listen(3000, () => {
-    console.log( 'listening on 300' );
+const mongoUrl = 'mongodb://root:root@ds033096.mlab.com:33096/custom-quotes';
+
+var db;
+
+MongoClient.connect(mongoUrl, (err, database) => {
+    if (err) { console.log(err) };
+
+    db = database;
+
+    app.listen(3000, () => {
+        console.log( 'listening on 300' );
+    })
 })
 
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
-    res.sendFile( __dirname + '/index.html' );
+    db.collection('quotes').find().toArray((err, result) => {
+        res.send(result);
+    })
 })
 
 app.post('/quotes', (req, res) => {
-    console.log(req.route.stack);
-    res.sendFile(__dirname + '/thanks.html')
+    db.collection('quotes').save(req.body, (err, result) => {
+        if (err) { console.log(err) };
+
+        console.log('Saved to Database :)')
+        res.redirect('/');
+    })
 })
 
